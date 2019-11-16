3757748a63922e79870ad375769c7a9eaff0b978
Richard <fahitch+github@gmail.com>
2019-11-16 19:34:53 +0000

    adds setup cards and highest card test

--- specs/testing_task_1_spec.rb
+++ specs/testing_task_1_spec.rb
@@ -5,16 +5,28 @@ require_relative('../card')
 
 class TestingTaskTest < Minitest::Test
 
-  def test_check_for_ace__not_ace()
+  def setup
     card1 = Card.new("clubs", "9")
+    card2 = Card.new("clubs", "1")
+    card2 = Card.new("hearts", "10")
+  end
+
+  def test_check_for_ace__not_ace()
     result = checkforAce(card1)
     assert_equal(result, false)
   end
 
-  def test_check_for_ace__ace()
-    card2 = Card.new("diamonds", "1")
-    result = checkforAce(card2)
-    assert_equal(result, true)
+  # def test_check_for_ace__ace()
+  #   result = checkforAce(card2)
+  #   assert_equal(result, true)
+  # end
+
+  def test_highest_card__card1_higher()
+    result = highest_card(card1, card2)
+    assert_equal(result, card1)
+  end
+
+
   end
 
 end
--- testing_task_1.rb
+++ testing_task_1.rb
@@ -1,3 +1,4 @@
+require_relative('card')
 class CardGame
 
 
@@ -9,19 +10,20 @@ class CardGame
     end
   end
 
-  dif highest_card(card1 card2)
-  if card1.value > card2.value
-    return card
-  else
-    return card2
+  def highest_card(card1, card2)
+    if card1.value > card2.value
+      return card
+    else
+      return card2
+    end
   end
-end
-end
 
-def self.cards_total(cards)
-  total
-  for card in cards
-    total += card.value
-    return "You have a total of" + total
+  def self.cards_total(cards)
+    total
+    for card in cards
+      total += card.value
+      return "You have a total of" + total
+    end
   end
+
 end
